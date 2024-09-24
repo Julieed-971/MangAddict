b79242dc12b4167beda63ac9e2f8564819a9c76b
Julieed-971 <julie.dedieu1@gmail.com>
2024-09-24 13:23:01 +0200

    Separated template for welcome page

--- my_project_directory/src/Controller/BasicController.php
+++ my_project_directory/src/Controller/BasicController.php
@@ -12,12 +12,18 @@ class BasicController extends AbstractController
     #[Route('/', name: 'app_basic')]
     public function index(): Response
     {
+        return $this->render('base.html.twig', [
+            'controller_name' => 'BasicController',
+        ]);
+    }
+    #[Route('/Bienvenue', name:'app_welcome')]
+    public function greet(WelcomeMessage $welcomeMessage): Response
+    {
         // Call the method on the message object
         $welcomeMessage = new WelcomeMessage();
         $welcomeMsg = $welcomeMessage->getWelcomeMessage('Username');
-        return $this->render('base.html.twig', [
-            'controller_name' => 'BasicController',
-            'welcomeMsg'=> $welcomeMsg,
+        return $this->render('welcome.html.twig', [
+            'welcomeMsg' => $welcomeMsg,
         ]);
     }
 }
