0d843e4e54f386ade29040e94a48e892abd5445c
Julieed-971 <julie.dedieu1@gmail.com>
2024-09-24 11:40:54 +0200

    Added welcome message service injected in controller to be displayed in template

--- my_project_directory/src/Controller/BasicController.php
+++ my_project_directory/src/Controller/BasicController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\WelcomeMessage;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,8 +12,12 @@ class BasicController extends AbstractController
     #[Route('/', name: 'app_basic')]
     public function index(): Response
     {
+        // Call the method on the message object
+        $welcomeMessage = new WelcomeMessage();
+        $welcomeMsg = $welcomeMessage->getWelcomeMessage('Username');
         return $this->render('base.html.twig', [
             'controller_name' => 'BasicController',
+            'welcomeMsg'=> $welcomeMsg,
         ]);
     }
 }
