6ba2b96c133fbf4b525e2a8676c85f7e48630522
Julieed-971 <julie.dedieu1@gmail.com>
2024-09-26 16:41:04 +0200

    Separated JS script for toggle color button

---
+++ assets/scripts/toggle_color.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$('#toggleColor').on('click', function() {
+		var currentBgColor = $('body').css('background-color');
+		var currentTextColor = $('body').css('color');
+	
+		$('body').css({
+			'background-color': currentTextColor,
+			'color': currentBgColor
+		});
+	});
+});
