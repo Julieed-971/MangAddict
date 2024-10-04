7a27a65c25780ba7354e54b24019c5b9bae2b43c
Julieed-971 <julie.dedieu1@gmail.com>
2024-10-04 12:15:58 +0200

    Created eventlistener to set lastconnected property to current datetime at each connection

--- src/EventListener/LastConnectionListener.php
+++ src/EventListener/LastConnectionListener.php
@@ -2,19 +2,28 @@
 
 namespace App\EventListener;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 
 final class LastConnectionListener
 {
+    public function __construct(private readonly EntityManagerInterface $manager)
+    {
+    }
     #[AsEventListener(event: 'security.interactive_login')]
     public function onSecurityInteractiveLogin(InteractiveLoginEvent $event): void
     {
-        
         // Get the current connected user
+        $user = $event->getAuthenticationToken()->getUser();
 
-        // Get the current date and time
-
-        // Set the current date and time to the user
+        // Check if user is instance of User
+        if ($user instanceof User) {
+            // Update the lastconnectedat property with current datetime
+            $user->setLastConnectedAt(new \DateTimeImmutable());
+            // Update the current user lastconnectedat property in the database
+            $this->manager->flush();
+        }
     }
 }
