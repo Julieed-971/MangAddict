6431a94ab0f5f05a74c0592f39328ff2637d5c94
Julieed-971 <julie.dedieu1@gmail.com>
2024-10-10 15:52:14 +0200

    Created MangaAuthor entity with story and art role to properly handle manga authors during de-serialization

---
+++ src/Entity/MangaAuthor.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MangaAuthorRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MangaAuthorRepository::class)]
+class MangaAuthor
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'mangaAuthors')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Manga $manga = null;
+
+    #[ORM\ManyToOne(inversedBy: 'mangaAuthors')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Author $author = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $role = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getManga(): ?Manga
+    {
+        return $this->manga;
+    }
+
+    public function setManga(?Manga $manga): static
+    {
+        $this->manga = $manga;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?Author
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?Author $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): static
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+}
