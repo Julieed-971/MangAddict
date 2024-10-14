73885029ecd64ee674ef7adc856a351c39e60e7e
Julieed-971 <julie.dedieu1@gmail.com>
2024-10-14 11:29:51 +0200

    Removed unnecessary use statement

--- src/Command/ListInactiveUsersCommand.php
+++ src/Command/ListInactiveUsersCommand.php
@@ -4,7 +4,6 @@ namespace App\Command;
 
 use App\Repository\UserRepository;
 use InvalidArgumentException;
-use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
