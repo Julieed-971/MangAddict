1028da926419b6038cc48354f230ac88e6d110e5
Julieed-971 <julie.dedieu1@gmail.com>
2024-10-17 09:29:02 +0200

    New entity rating for mangas

---
+++ src/Entity/Manga/Rating.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Entity\Manga;
+
+use App\Entity\User;
+use App\Repository\Manga\RatingRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RatingRepository::class)]
+class Rating
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $note = null;
+
+    #[ORM\ManyToOne(inversedBy: 'ratings')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
+    #[ORM\ManyToOne(inversedBy: 'ratings')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Manga $manga = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNote(): ?int
+    {
+        return $this->note;
+    }
+
+    public function setNote(int $note): static
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getManga(): ?Manga
+    {
+        return $this->manga;
+    }
+
+    public function setManga(?Manga $manga): static
+    {
+        $this->manga = $manga;
+
+        return $this;
+    }
+}
