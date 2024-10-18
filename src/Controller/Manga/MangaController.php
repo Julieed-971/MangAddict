718fdab3b35d91957d8271718bfc495404bc4fd1
Julieed-971 <julie.dedieu1@gmail.com>
2024-10-18 19:32:11 +0200

    Updated manga controller to handle rating and review form

--- src/Controller/Manga/MangaController.php
+++ src/Controller/Manga/MangaController.php
@@ -6,6 +6,7 @@ use App\Entity\Manga\Manga;
 use App\Entity\Manga\Rating;
 use App\Entity\Manga\Review;
 use App\Form\RatingType;
+use App\Form\ReviewType;
 use App\Repository\Manga\RatingRepository;
 use App\Repository\Manga\ReviewRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -29,25 +30,39 @@ class MangaController extends AbstractController
 	{
 		// Get currently connected user's manga rating if exist
 		$currentRating = null;
+		$currentReview = null;
 		if ($this->getUser()) {
 			$currentRating = $this->ratingRepository->findOneBy([
-				'user' =>$this->getUser(),
+				'user' => $this->getUser(),
+				'manga' => $manga]);
+			$currentReview = $this->reviewRepository->findOneBy([
+				'user' => $this->getUser(),
 				'manga' => $manga]);
 		}
 
-		// Create a form for rating only if rating is null
+		// Create a form for rating 
 		$rating = new Rating();
-	
+		
+		// Create a review form
+		$review = new Review();
+
 		// If user already rated set the rating
 		if ($currentRating) {
 			$rating->setNote($currentRating->getNote());
 		}
 
-		$form = $this->createForm(RatingType::class, $rating);
+		// If user already reviewed the manga, set the review
+		if ($currentReview) {
+			$review->setContent($currentReview->getContent());
+		}
+
+		$ratingForm = $this->createForm(RatingType::class, $rating);
+		$reviewForm = $this->createForm(ReviewType::class, $review);
 		// Handle form submission
-		$form->handleRequest($request);
+		$ratingForm->handleRequest($request);
+		$reviewForm->handleRequest($request);
 			
-		if ($form->isSubmitted() && $form->isValid()) { 
+		if ($ratingForm->isSubmitted() && $ratingForm->isValid()) { 
 			$user = $this->getUser();
 			$rating->setUser($user);
 			$rating->setManga($manga);
@@ -57,17 +72,33 @@ class MangaController extends AbstractController
 				$currentRating->setNote($rating->getNote());
 				$entityManager->persist($currentRating);
 			} else {
-			$entityManager->persist($rating);
+				$entityManager->persist($rating);
+			}
+		// $entityManager->flush();
+		
+		if ($reviewForm->isSubmitted() && $reviewForm->isValid()) {
+			$user = $this->getUser();
+			$review->setUser($user);
+			$review->setManga($manga);
+
+			// Update or create review and save it to database
+			if ($currentReview) {
+				$currentReview->setContent($review->getContent());
+				$entityManager->persist($currentReview);
+			} else {
+				$entityManager->persist($review);
 			}
 		$entityManager->flush();
-
+		}
 		return $this->redirectToRoute('app_manga_display', ['id' => $manga->getId()]);
 		}
 
 		return $this->render('/manga/display.html.twig', [
 			'manga' => $manga,
-			'ratingForm' => $form,
+			'ratingForm' => $ratingForm,
+			'reviewForm' => $reviewForm,
 			'currentRating' => $currentRating ? $currentRating->getNote() : null,
+			'currentReview' => $currentReview ? $currentReview->getContent() : null,
 		]);
 	}
 }
