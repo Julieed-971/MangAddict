fa8535b1d259f0ea54b50dcc96902eb25113f638
Julieed-971 <julie.dedieu1@gmail.com>
2024-10-21 18:30:38 +0200

    Refactorize and added update and delete functionnality to review

--- src/Controller/Manga/MangaController.php
+++ src/Controller/Manga/MangaController.php
@@ -29,77 +29,78 @@ class MangaController extends AbstractController
 	public function display(?Manga $manga, Request $request, EntityManagerInterface $entityManager): Response
 	{
 		// Get currently connected user's manga rating if exist
-		$currentRating = null;
-		$currentReview = null;
-		if ($this->getUser()) {
-			$currentRating = $this->ratingRepository->findOneBy([
-				'user' => $this->getUser(),
+		$rating = null;
+		$review = null;
+		$user = $this->getUser();
+		if ($user) {
+			$rating = $this->ratingRepository->findOneBy([
+				'user' => $user,
 				'manga' => $manga]);
-			$currentReview = $this->reviewRepository->findOneBy([
-				'user' => $this->getUser(),
+			$review = $this->reviewRepository->findOneBy([
+				'user' => $user,
 				'manga' => $manga]);
 		}
 
-		// Create a form for rating 
-		$rating = new Rating();
-		
-		// Create a review form
-		$review = new Review();
-
-		// If user already rated set the rating
-		if ($currentRating) {
-			$rating->setNote($currentRating->getNote());
-		}
-
-		// If user already reviewed the manga, set the review
-		if ($currentReview) {
-			$review->setContent($currentReview->getContent());
-		}
-
+		// Create the forms
 		$ratingForm = $this->createForm(RatingType::class, $rating);
 		$reviewForm = $this->createForm(ReviewType::class, $review);
+
 		// Handle form submission
 		$ratingForm->handleRequest($request);
 		$reviewForm->handleRequest($request);
 			
 		if ($ratingForm->isSubmitted() && $ratingForm->isValid()) { 
-			$user = $this->getUser();
-			$rating->setUser($user);
-			$rating->setManga($manga);
+			if (!$rating) {
+				$rating = new Rating();
+				$rating->setUser($user);
+				$rating->setManga($manga);
+			}
 
 			// Update or create rating and save to database
-			if ($currentRating) {
-				$currentRating->setNote($rating->getNote());
-				$entityManager->persist($currentRating);
-			} else {
-				$entityManager->persist($rating);
-			}
+			$rating->setUser($user);
+			$rating->setManga($manga);
+			$rating->setNote($rating->getNote());
+			$entityManager->persist($rating);
 			$entityManager->flush();
-			return $this->redirectToRoute('app_manga_display', ['id' => $manga->getId()]);		
-		}
+			$this->addFlash('success', 'Votre note a été ajoutée.');
+			return $this->redirectToRoute('app_manga_display', ['id' => $manga->getId()]);
+			}
+					
 		
 		if ($reviewForm->isSubmitted() && $reviewForm->isValid()) {
-			$user = $this->getUser();
+			if (!$review) {
+				$review = new Review();
+				$review->setUser($user);
+				$review->setManga($manga);
+			}
 			$review->setUser($user);
 			$review->setManga($manga);
+			$review->setContent($reviewForm->get('content')->getData());
+			$updatedAt = new \DateTimeImmutable();
+			$review->setUpdatedAt($updatedAt);
 
-			// Update or create review and save it to database
-			if ($currentReview) {
-				$currentReview->setContent($review->getContent());
-				$entityManager->persist($currentReview);
-			} else {
-				$entityManager->persist($review);
-			}
+			$entityManager->persist($review);
 			$entityManager->flush();
+			$this->addFlash('success', 'Votre critique a été ajoutée.');
 			return $this->redirectToRoute('app_manga_display', ['id' => $manga->getId()]);
 		}
-
 		return $this->render('/manga/display.html.twig', [
 			'manga' => $manga,
 			'ratingForm' => $ratingForm,
 			'reviewForm' => $reviewForm,
-			'currentRating' => $currentRating ? $currentRating->getNote() : null,
-			'currentReview' => $currentReview ? $currentReview : null,
+			'rating' => $rating ? $rating->getNote() : null,
+			'review' => $review ? $review : null,
 		]);
+	}
+
+	#[Route('/review/{id}/delete', name: 'app_review_delete', methods: ['POST'])]
+	public function delete(Request $request, Review $review, EntityManagerInterface $entityManager) : Response
+	{
+		if ($this->isCsrfTokenValid('delete' . $review->getId(), $request->request->get('_token'))) {
+			$entityManager->remove($review);
+			$entityManager->flush();
+			$this->addFlash('success', 'Votre critique a été supprimée.');
+		}
+	return $this->redirectToRoute('app_manga_display', ['id' => $review->getManga()->getId()]);
 	}
 }
