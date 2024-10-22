7f02531cb1c1c204ddfa880be140f38506de8f41
Julieed-971 <julie.dedieu1@gmail.com>
2024-10-22 23:39:38 +0200

    Added pagination functionality with knp paginator

--- src/Repository/Manga/MangaRepository.php
+++ src/Repository/Manga/MangaRepository.php
@@ -3,8 +3,10 @@
 namespace App\Repository\Manga;
 
 use App\Entity\Manga\Manga;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Knp\Component\Pager\PaginatorInterface;
 
 /**
  * @extends ServiceEntityRepository<Manga>
@@ -26,6 +28,29 @@ class MangaRepository extends ServiceEntityRepository
 		return $this->findOneBy(['name' => $name]);
 	}
 
+	public function getPaginatedMangas($filterData, PaginatorInterface $paginator, $page = 1, $limit = 16)
+	{
+		$queryBuilder = $this->createQueryBuilder('m');
+
+		if (!empty($filterData['type'])) {
+			$queryBuilder->andWhere('m.type LIKE :type')
+						 ->setParameter('type', '%' . $filterData['type'] . '%');
+		}
+		if (!empty($filterData['genre'])) {
+			$queryBuilder->andWhere('m.genres LIKE :genre')
+						 ->setParameter('genre', '%' . $filterData['genre'] . '%');
+		}
+		if (!empty($filterData['author'])) {
+			$queryBuilder->andWhere('m.mangaAuthor LIKE :author')
+						 ->setParameter('author', '%' . $filterData['author'] . '%');
+		}
+
+		return $paginator->paginate(
+			$queryBuilder,
+			$page,
+			$limit
+			);
+	}
 	// FindByAuthor
 	// FindByStatus
 	// FindByGenre
